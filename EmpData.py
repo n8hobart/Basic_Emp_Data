98c1a3d158827eadc229f80c254b62f82297e8f8
n8hobart <44904211+n8hobart@users.noreply.github.com>
2018-11-09 13:28:31 -0700

    Update EmpData.py

--- EmpData.py
+++ EmpData.py
@@ -1,10 +1,12 @@
+#Creating employee class
 class Employee:
     def __init__(self, name, empID):
         self.name = name
         self.empID = empID
         self.salary= None
     def calcSalary(self):
-        raise NotImplementedErroe("Not correct")
+        raise NotImplementedError("Not correct")
+#Hourly employee data: Inherits from Employee Class
 class Hourly(Employee):
     def __init__(self, name, empID):
         Employee.__init__(self, name, empID)
@@ -14,6 +16,7 @@ class Hourly(Employee):
         self.hoursWorked = input("Weekly hours: ")
         self.payRate = input("What is the hourly rate: ")
         self.salary = int(self.hoursWorked)*float(self.payRate)*52
+#Annual employee data: Inherits from Employee Class
 class Yearly(Employee):
     def __init__(self, name, empID):
         Employee.__init__(self, name, empID)
@@ -24,7 +27,7 @@ class Yearly(Employee):
         self.years = input("Years Worked: ")
         self.salary = int(self.startingWage)+(int(self.startingWage)*.15*int(self.years))
  
-
+#Creating list to store employee data
 employees = list()
 employees.append(Hourly("Nathan",1))
 employees.append(Yearly("Adriaunnah",2))
